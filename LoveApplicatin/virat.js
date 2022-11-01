3803d9ca5e926dee201baa218eab6aeadfa44358
Chandru <chandrutallikeri319@gmail.com>
2022-11-01 19:18:28 +0530

    Updated
    It's about love application form
    for fun 😍💖😍

---
+++ LoveApplicatin/virat.js
@@ -0,0 +1,21 @@
+function yes()
+{
+    document.getElementById("form").style.display="none";
+    document.getElementById("yes").style.display="block";
+    document.getElementById("no").style.display="none";
+}
+
+function no()
+{
+    // document.getElementById("form").style.display="none";
+    document.getElementById("no").style.display="block";
+}
+
+function sub()
+{
+    document.getElementById("form").style.display="none";
+    document.getElementById("no").style.display="none";
+    document.getElementById("brk").style.display="none";
+    document.getElementById("sub").style.display="block";
+    // document.getElementById("love").style.display="block";
+}
